9e56ddddc0b921aea577eb39df7545cf25600423
Daniel Salhuana <dars1903@gmail.com>
2025-11-22 14:51:41 +0100

    Added Project

---
+++ Digidentity/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Digidentity
+//
+//  Created by Daniel Salhuana on 22/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ DigidentityTests/DigidentityTests.swift
@@ -0,0 +1,15 @@
+//
+//  DigidentityTests.swift
+//  DigidentityTests
+//
+//  Created by Daniel Salhuana on 22/11/25.
+//
+
+import Testing
+@testable import Digidentity
+
+struct DigidentityTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
